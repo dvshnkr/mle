d50d55619373179aa543a44fedd604b4e80f7fd9
Dev Shankar N <30773393+dvshnkr@users.noreply.github.com>
2023-08-25 07:58:20 +0530

    linters and tests

---
+++ src/hello_world.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+var1 = {"foo": "bar"}
+
+if __name__ == "__main__":
+    print("hello world")
+    print(np.__version__)
---
+++ src/tests/test_hello.py
@@ -0,0 +1,8 @@
+import hello_world
+
+
+def test_var1():
+    """
+    Tests the hello_world.py file.
+    """
+    assert hello_world.var1["foo"] == "bar"
