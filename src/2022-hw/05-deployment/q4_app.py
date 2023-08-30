6128108db82f12648c46794e3531c8eed50659ed
Dev Shankar N <30773393+dvshnkr@users.noreply.github.com>
2023-08-31 05:05:21 +0530

    fixes duplicate code
    uses helper functions to reduce redundancy in code

--- src/2022-hw/05-deployment/q4_app.py
+++ src/2022-hw/05-deployment/q4_app.py
@@ -1,42 +1,15 @@
-import pickle
-
-from flask import Flask, jsonify, request
+from endpoint_helper import predict_endpoint
+from flask import Flask
 
 app = Flask(__name__)
-
-
-def load_model():
-    """
-    Loads model to make predictions.
-    """
-    # loads DictVectorizer and model
-    with open("./dv.bin", "rb") as f_in:
-        dict_vzer = pickle.load(f_in)
-    with open("./model1.bin", "rb") as f_in:
-        model = pickle.load(f_in)
-    return dict_vzer, model
-
-
-def predict(client: dict) -> float:
-    """
-    Returns a prediction for the client.
-    """
-    dict_vzer, model = load_model()
-    client_enc = dict_vzer.transform(client)  # transforms data
-    # predict probably of client belonging to positive class
-    y_hat = model.predict_proba(client_enc)[:, 1][0]
-    return y_hat
 
 
 @app.route("/predict", methods=["POST"])
 def predict_route():
     """
-    Creates an endpoint for users to send information.
-    Returns the prediction made by the model.
+    Creates the route using the imported endpoint function.
     """
-    client = request.get_json()  # receives JSON input with features
-    prediction = predict(client)  # makes prediction
-    return jsonify({"prediction": prediction})  # returns prediction in JSON format
+    return predict_endpoint("model1.bin")
 
 
 if __name__ == "__main__":
